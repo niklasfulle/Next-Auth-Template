2ea8a30ac96ee58998a538351920748104cbd284
Niklas Fulle <36989748+Salkin263@users.noreply.github.com>
2023-09-07 22:30:51 +0200

    init

---
+++ prisma/migrations/20230907202847_/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the `ApiKey` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `ApiRequest` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "ApiKey";
+
+-- DropTable
+DROP TABLE "ApiRequest";
