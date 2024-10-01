ec23a52618b2f1dadea3ec02ecc41e4e780bf2be
Niklas Fulle <36989748+Salkin263@users.noreply.github.com>
2024-10-01 23:51:30 +0200

    worked on callbacks and google and github provider

--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,6 @@
 import Credentials from "next-auth/providers/credentials"
+import Github from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
 import type { NextAuthConfig } from "next-auth"
 import bcrypt from "bcryptjs"
 
@@ -6,22 +8,32 @@ import { LoginSchema } from "@/schemas"
 import { getUserByEmail } from "./data/user"
 
 export default {
-  providers: [Credentials({
-    async authorize(credentials) {
-      const validatedFields = LoginSchema.safeParse(credentials)
-      if (validatedFields.success) {
-        const { email, password } = validatedFields.data
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET
+    }),
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials)
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data
 
-        const user = await getUserByEmail(email)
-        if (!user || !user.hashedPassword) return null
+          const user = await getUserByEmail(email)
+          if (!user || !user.hashedPassword) return null
 
-        const passwordMatch = await bcrypt.compare(password, user.hashedPassword)
+          const passwordMatch = await bcrypt.compare(password, user.hashedPassword)
 
-        if (passwordMatch) return user
-      }
+          if (passwordMatch) return user
+        }
 
-      return null
+        return null
 
-    },
-  })]
+      },
+    })
+  ]
 } satisfies NextAuthConfig
--- auth.ts
+++ auth.ts
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
+
+import { getUserById } from "@/data/user"
 import { db } from "@/lib/db"
 import authConfig from "./auth.config"
+import { UserRole } from "@prisma/client"
 
 export const {
   handlers: { GET, POST },
@@ -9,6 +12,45 @@ export const {
   signIn,
   signOut
 } = NextAuth({
+  callbacks: {
+    /*
+    async signIn({ user }) {
+      let existingUser
+      if (user.id) {
+        existingUser = await getUserById(user.id)
+      } else {
+        return false
+      }
+
+      if (!existingUser || !existingUser.emailVerified) {
+        return false
+      }
+
+      return true
+    },*/
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole
+      }
+
+      return session
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub)
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role
+
+      return token
+    }
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
