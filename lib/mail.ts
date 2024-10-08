20e51282ce5f62b1978499c1c13b03c5a6a5f493
Niklas Fulle <36989748+Salkin263@users.noreply.github.com>
2024-10-08 19:36:26 +0200

    changed resend to nodemailer

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,35 +1,89 @@
-import { Resend } from "resend"
-
-const resend = new Resend(process.env.RESEND_API_KEY)
+import nodemailer from 'nodemailer';
 
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`
 
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
+  const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+      user: process.env.NODEMAILER_EMAIL,
+      pass: process.env.NODEMAILER_PW,
+    },
+    secure: true,
+  });
+
+  const mailOptions = {
+    from: process.env.NODEMAILER_EMAIL,
     to: email,
     subject: "Confirm your email",
-    html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`
-  })
+    text: `Click ${confirmLink} to confirm email.`,
+    html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
+  };
+
+  transporter.sendMail(mailOptions, function (error: any) {
+    if (error) {
+      throw new Error(error);
+    } else {
+      return true;
+    }
+  });
 }
 
 export const sendResetPasswordEmail = async (email: string, token: string) => {
   const confirmLink = `http://localhost:3000/auth/new-password?token=${token}`
 
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
+  const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+      user: process.env.NODEMAILER_EMAIL,
+      pass: process.env.NODEMAILER_PW,
+    },
+    secure: true,
+  });
+
+  const mailOptions = {
+    from: process.env.NODEMAILER_EMAIL,
     to: email,
     subject: "Reset your password",
-    html: `<p>Click <a href="${confirmLink}">here</a> to reset your password.</p>`
-  })
+    text: `Click ${confirmLink} to reset your password.`,
+    html: `<p>Click <a href="${confirmLink}">here</a> to reset your password.</p>`,
+  };
+
+  transporter.sendMail(mailOptions, function (error: any) {
+    if (error) {
+      throw new Error(error);
+    } else {
+      return true;
+    }
+  });
 }
 
 export const sendTwoFactorEmail = async (email: string, token: string) => {
+  const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+      user: process.env.NODEMAILER_EMAIL,
+      pass: process.env.NODEMAILER_PW,
+    },
+    secure: true,
+  });
 
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
+  const mailOptions = {
+    from: process.env.NODEMAILER_EMAIL,
     to: email,
     subject: "2FA Code",
+    text: `Your 2FA code: ${token}`,
     html: `<p>Your 2FA code: ${token}</p>`
-  })
+  };
+
+  transporter.sendMail(mailOptions, function (error: any) {
+    if (error) {
+      throw new Error(error);
+    } else {
+      return true;
+    }
+  });
 }
